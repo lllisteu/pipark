67135ef0b8726f84c47d19e0fe5469186927e4e7
lllisteu <lllisteu@users.noreply.github.com>
2022-05-14 09:03:01 +0200

    Add Host#cpu_temperature, #os_release, CE doc, refine #inspect

--- lib/pipark/host.rb
+++ lib/pipark/host.rb
@@ -30,9 +30,9 @@ module Pipark
       @address = address
     end
 
-    # Returns a programmer-friendly representation of the Host.
+    # Returns a programmer-friendly representation of the host.
     def inspect
-      "Host '#{address}'"
+      %Q(Pipark::Host "#{address}")
     end
 
     # Flushes cached attributes.
@@ -40,24 +40,35 @@ module Pipark
       @cache = {}
     end
 
-    # Returns true if the Host is localhost.
+    # Returns true if the host is localhost.
     def localhost?
       @localhost ||= %w(localhost 127.0.0.1 ::1).include? address
     end
 
-    # Returns true if the Host can be pinged.
+    # Returns true if the host can be pinged.
     def pingable?
       Pipark.pingable? address
     end
 
-    # Returns the Host's hostname.
+    # Returns the host's hostname.
     def hostname
       cache['hostname'] ||= file_read('/etc/hostname').chomp
     end
 
-    # Returns the Host's Raspberry Pi model.
+    # Returns the host's Raspberry Pi model.
     def model
       cache['model'] ||= file_read('/proc/device-tree/model').chop
+    end
+
+    # Returns the hosts's CPU temperature.
+    def cpu_temperature
+      file_read('/sys/class/thermal/thermal_zone0/temp').chomp.to_i
+    end
+
+    # Returns information about the host's operating system.
+    def os_release
+      cache['os-release'] ||=
+        file_read('/etc/os-release').lines.map { |l| l.chomp.split('=') }.map { |k,v| [k,v.chomp.gsub('"','')] }.to_h
     end
 
     private
